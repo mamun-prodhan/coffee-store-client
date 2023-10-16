6f8c43843ecc38a33f26814de1777eecdc63c95e
mamun-prodhan <mamunpr9@gmail.com>
2023-10-16 22:55:38 +0600

    handleaddcoffee inplemented

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h2>This is update coffee page</h2>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
